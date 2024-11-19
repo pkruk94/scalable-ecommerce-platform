a1d2945cd4802d04428d781286050670fb588931
pkruk <pkruk94@gmail.com>
2024-11-19 23:07:06 +0100

    Initial commit

---
+++ services/scalable-ecommerce-platform-config-server/src/main/java/com/pkruk/ecommerce/config/ScalableEcommercePlatformConfigServerApplication.java
@@ -0,0 +1,13 @@
+package com.pkruk.ecommerce.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScalableEcommercePlatformConfigServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScalableEcommercePlatformConfigServerApplication.class, args);
+    }
+
+}
