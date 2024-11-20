398d605ee60fa9f8ea6fed485af5187f6ae488cb
pkruk <pkruk94@gmail.com>
2024-11-20 11:52:29 +0100

    Added implementation for discovery service

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # scalable-ecommerce-platform
 Simple project for the purpose of learning
+
+# todo
+- discovery service + konfiguracja + zobaczyć czy działa 
+- Docker - ciag dalszy kursu
--- services/scalable-ecommerce-platform-discovery-service/src/main/java/com/pkruk/ecommerce/discovery/ScalableEcommercePlatformDiscoveryServiceApplication.java
+++ services/scalable-ecommerce-platform-discovery-service/src/main/java/com/pkruk/ecommerce/discovery/ScalableEcommercePlatformDiscoveryServiceApplication.java
@@ -2,8 +2,10 @@ package com.pkruk.ecommerce.discovery;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class ScalableEcommercePlatformDiscoveryServiceApplication {
 
     public static void main(String[] args) {
