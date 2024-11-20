295afcc107abc1ddf703187cab7df3293496ee8d
pkruk <pkruk94@gmail.com>
2024-11-20 11:41:34 +0100

    Initial commit

---
+++ services/scalable-ecommerce-platform-discovery-service/src/main/java/com/pkruk/ecommerce/discovery/ScalableEcommercePlatformDiscoveryServiceApplication.java
@@ -0,0 +1,13 @@
+package com.pkruk.ecommerce.discovery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScalableEcommercePlatformDiscoveryServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScalableEcommercePlatformDiscoveryServiceApplication.class, args);
+    }
+
+}
---
+++ services/scalable-ecommerce-platform-discovery-service/src/test/java/com/pkruk/ecommerce/discovery/ScalableEcommercePlatformDiscoveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pkruk.ecommerce.discovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScalableEcommercePlatformDiscoveryServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
