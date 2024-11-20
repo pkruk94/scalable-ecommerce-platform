eb1ade4ad3666714110eb44eff6ea3b6cf6a9938
pkruk <pkruk94@gmail.com>
2024-11-21 00:18:20 +0100

    Deleted annotation

--- services/scalable-ecommerce-platform-product-service/src/main/java/com/pkruk/ecommerce/product/ProductRepository.java
+++ services/scalable-ecommerce-platform-product-service/src/main/java/com/pkruk/ecommerce/product/ProductRepository.java
@@ -1,11 +1,9 @@
 package com.pkruk.ecommerce.product;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
-@Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
     List<Product> findAllByIdInOrderById(List<Long> productIds);
 }
