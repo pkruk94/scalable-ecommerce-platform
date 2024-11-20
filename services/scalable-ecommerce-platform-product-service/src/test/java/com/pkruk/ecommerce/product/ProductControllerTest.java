72e0d162742f69a8ab8b951fa3c72b49b5841403
pkruk <pkruk94@gmail.com>
2024-11-21 00:36:07 +0100

    Try H2

--- services/scalable-ecommerce-platform-product-service/src/test/java/com/pkruk/ecommerce/product/ProductControllerTest.java
+++ services/scalable-ecommerce-platform-product-service/src/test/java/com/pkruk/ecommerce/product/ProductControllerTest.java
@@ -12,6 +12,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.math.BigDecimal;
@@ -30,6 +31,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
         ProductController.class,
         GlobalControllerAdvice.class
 })
+@ActiveProfiles("test")
 public class ProductControllerTest {
 
     @Autowired
