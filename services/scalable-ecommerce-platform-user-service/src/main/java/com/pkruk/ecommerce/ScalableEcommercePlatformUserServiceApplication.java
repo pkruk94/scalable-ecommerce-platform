9145d04ea8509a9d2e36b548ae5c6fe1a0f8bb6b
pkruk <pkruk94@gmail.com>
2024-11-21 00:48:51 +0100

    Initial commit for User Service

---
+++ services/scalable-ecommerce-platform-user-service/src/main/java/com/pkruk/ecommerce/ScalableEcommercePlatformUserServiceApplication.java
@@ -0,0 +1,13 @@
+package com.pkruk.ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScalableEcommercePlatformUserServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScalableEcommercePlatformUserServiceApplication.class, args);
+    }
+
+}
