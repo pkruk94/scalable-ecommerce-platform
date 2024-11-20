33066b5f25b52504e845abba0034b36be53b77b2
pkruk <pkruk94@gmail.com>
2024-11-20 23:49:47 +0100

    Updated github actions

--- services/scalable-ecommerce-platform-product-service/src/test/java/com/pkruk/ecommerce/ScalableEcommercePlatformProductServiceApplicationTests.java
+++ services/scalable-ecommerce-platform-product-service/src/test/java/com/pkruk/ecommerce/ScalableEcommercePlatformProductServiceApplicationTests.java
@@ -2,10 +2,10 @@ package com.pkruk.ecommerce;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest
-@ActiveProfiles("test")
+@SpringBootTest(properties = {
+		"spring.autoconfigure.exclude=org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration,org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration"
+})
 class ScalableEcommercePlatformProductServiceApplicationTests {
 
 	@Test
